4b5087873d1c43a3ac2ed67cff747356554bccaf
itsmraga-hub <ragawilliam570@gmail.com>
2023-08-09 11:57:14 +0300

    Task 1: Allow user check with default value

---
+++ 0x00-MYSQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- creates a table users
+-- with id, email and name columns
+CREATE TABLE IF NOT EXISTS users (
+	id SERIAL PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country VARCHAR(2) NOT NULL DEFAULT 'US',
+	CHECK (country IN ('US', 'CO', 'TN'))
+);
