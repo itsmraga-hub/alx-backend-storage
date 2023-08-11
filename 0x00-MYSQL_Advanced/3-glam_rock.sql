0a36d2e73dc1602a725fd127714faf2b22bce233
itsmraga-hub <ragawilliam570@gmail.com>
2023-08-11 18:23:28 +0300

    Task 3

--- 0x00-MYSQL_Advanced/3-glam_rock.sql
+++ 0x00-MYSQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
-SELECT band_name, 
-       EXTRACT(YEAR FROM DATE '2022-01-01' - split) - EXTRACT(YEAR FROM formed) AS lifespan
+--  lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
-
