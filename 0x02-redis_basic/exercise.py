40d454db527c1f1a211ac15e9773a35058d90c7e
root <root@Raga>
2023-08-16 13:36:33 +0300

    Task 0: fix pycode style error

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """
-    Create a Cache class. In the __init__ method, store an instance of the Redis
-    client as a private variable named _redis (using redis.Redis()) and
-    flush the instance using flushdb.
+    Create a Cache class. In the __init__ method, store an instance of the
+    Redis client as a private variable named _redis (using redis.Redis())
+    and flush the instance using flushdb.
 """
 import redis
 from typing import Union
@@ -17,13 +17,10 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
             Store method that takes a data argument and returns a string
         """
-
         key = uuid.uuid4()
         self._redis.set(str(key), data)
         return str(key)
-
