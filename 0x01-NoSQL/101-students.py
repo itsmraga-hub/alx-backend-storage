7c39a6fe7f0bed451c764eaea0e5ce1072872dcc
root <root@Raga>
2023-08-18 20:54:01 +0300

    Task 14 top student

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Write a Python function that returns all students sorted by average score:
+
+Prototype: def top_students(mongo_collection):
+mongo_collection will be the pymongo collection object
+The top must be ordered
+The average score must be part of each item returns with key = averageScore
+"""
+
+
+def top_students(mongo_collection):
+    # sourcery skip: inline-immediately-returned-variable
+    """ Returns all students sorted by average score """
+    top_student = mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ])
+
+    return top_student
